cf89a5eeae2e722b296c0eda2cc42737ee7ff636
Eduardo Hertz de Almeida Gomes <hertz@AdaLovelace-desktop.(none)>
2009-11-27 15:36:28 -0200

    História: 'Popular banco de dados 2' finalizada.

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,3 +53,56 @@ open("#{Rails.root}/db/seeds_files/motoristas_seed.txt") do |motoristas|
   end
 end
 
+Solicitante.delete_all
+open("#{Rails.root}/db/seeds_files/solicitantes_seed.txt") do |solicitantes|
+  solicitantes.read.each do |s|
+    if s != "\n"
+      nome, email, matricula, cargo = s.chomp.split("|")
+      Solicitante.create!(:nome => nome,
+                          :email => email,
+                          :matricula => matricula,
+                          :cargo => cargo)
+    end
+  end
+end
+
+Usuario.delete_all
+open("#{Rails.root}/db/seeds_files/usuarios_seed.txt") do |usuarios|
+  usuarios.read.each do |u|
+    if u != "\n"
+      nome, email, login, password, role = u.chomp.split("|")
+      @usuario = Usuario.new({:nome => nome, :email => email, :login => login, :password => password, :password_confirmation => password})
+      @usuario.save
+      @usuario.has_role! role
+    end
+  end
+end
+
+Requisicao.delete_all
+open("#{Rails.root}/db/seeds_files/requisicoes_seed.txt") do |requisicoes|
+  requisicoes.read.each do |r|
+    if r != "\n"
+      sol, categoria_veiculo, cargo, telefone, celular, laboratorio, predio, andar, sala, objetivo, outros, passageiros, roteiro, observacao = r.chomp.split("|")
+      data = Date.tomorrow.tomorrow
+      solicitante = Solicitante.find_by_nome(sol)
+      categoria_de_veiculo = CategoriaDeVeiculo.find_by_nome(categoria_veiculo)
+      puts categoria_de_veiculo.id
+      Requisicao.create!(:solicitante_id => solicitante.id,
+                          :categoria_de_veiculo_id => categoria_de_veiculo.id,
+                          :cargo_ou_funcao => cargo,
+                          :telefone_ou_ramal => telefone,
+                          :celular => celular,
+                          :laboratorio_ou_setor => laboratorio,
+                          :predio => predio,
+                          :andar => andar,
+                          :sala => sala,
+                          :data_de_reserva => data,
+                          :objetivo_da_reserva => objetivo,
+                          :outros => outros,
+                          :nome_telefone_passageiros => passageiros,
+                          :roteiro_da_agenda => roteiro,
+                          :observacao => observacao)
+    end
+  end
+end
+
