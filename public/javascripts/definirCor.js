e54f27906ce23195fb3cfd85b86a1c16860fa1a1
Hugo Henriques Maia Vieira <hugouenf@gmail.com>
2010-02-23 22:03:53 -0300

    Refatorado script definirCor.

--- public/javascripts/definirCor.js
+++ public/javascripts/definirCor.js
@@ -1,27 +1,41 @@
-    function definirCor()
-    {
-      var url = document.location.pathname
+function definir_aba(aba)
+{
+    abas = ["opcao_requisicao", "opcao_viagem", "opcao_base_de_dados"]
 
-      if (url.search(/requisic(ao|oes)/) != -1)
-      {
-        document.getElementById("opcao_viagem").style.backgroundColor = '#DCDCDC'
-        document.getElementById("opcao_base_de_dados").style.backgroundColor = '#DCDCDC'
-        document.getElementById("opcao_viagem").style.borderBottom = '#DCDCDC'
-        document.getElementById("opcao_base_de_dados").style.borderBottom = '#DCDCDC'
-      }
-      else if (url.search(/viage(ns|m)/) != -1)
-      {
-        document.getElementById("opcao_requisicao").style.backgroundColor = '#DCDCDC'
-        document.getElementById("opcao_base_de_dados").style.backgroundColor = '#DCDCDC'
-        document.getElementById("opcao_requisicao").style.borderBottom = '#DCDCDC'
-        document.getElementById("opcao_base_de_dados").style.borderBottom = '#DCDCDC'
-      }
-      else if (url.search(/base_de_dados/) != -1)
-      {
-        document.getElementById("opcao_requisicao").style.backgroundColor = '#DCDCDC'
-        document.getElementById("opcao_viagem").style.backgroundColor = '#DCDCDC'
-        document.getElementById("opcao_requisicao").style.borderBottom = '#DCDCDC'
-        document.getElementById("opcao_viagem").style.borderBottom = '#DCDCDC'
-      }
+    for (i = 0; i < abas.length; i++)
+    {
+        if (abas[i] != aba)
+        {
+            document.getElementById(abas[i]).style.backgroundColor = '#DCDCDC'
+            document.getElementById(abas[i]).style.borderBottom = '#DCDCDC'
+        }
     }
+}
+
+function definirCor()
+{
+    var url = document.location.pathname
+
+    if (url.search(/requisic(ao|oes)/) != -1)
+    {
+        definir_aba("opcao_requisicao")
+    }
+    else if (url.search(/viage(ns|m)/) != -1)
+    {
+        definir_aba("opcao_viagem")
+    }
+    else if (
+    (url.search(/base_de_dados/) != -1) ||
+    (url.search(/categoria_de_veiculos/) != -1) ||
+    (url.search(/combustiveis/) != -1) ||
+    (url.search(/solicitantes/) != -1) ||
+    (url.search(/motoristas/) != -1) ||
+    (url.search(/\/veiculos/) != -1) ||
+    (url.search(/usuario/) != -1) ||
+    (url.search(/motivo/) != -1)
+    )
+    {
+        definir_aba("opcao_base_de_dados")
+    }
+}
 
