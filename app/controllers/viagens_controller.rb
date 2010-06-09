51359d9802a9b3d4d5bfdb7043a68878e2926d07
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2010-06-09 15:14:56 -0300

    viagens_controller refatorado

--- app/controllers/viagens_controller.rb
+++ app/controllers/viagens_controller.rb
@@ -1,6 +1,4 @@
 class ViagensController < ApplicationController
-  # GET /viagens
-  # GET /viagens.xml
 
   access_control do
     allow :admin
@@ -11,45 +9,24 @@ class ViagensController < ApplicationController
 
   def index
     @viagens = Viagem.all(:conditions => "estado = '" + Viagem::AGUARDANDO + "'", :order => "data_partida ASC")
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @viagens }
-    end
   end
 
   def filtrar
     @filtro = params[:filtro]
     @viagens = Viagem.filtrar(@filtro)
     render :action => "index"
-
   end
 
-  # GET /viagens/1
-  # GET /viagens/1.xml
   def show
     @viagem = Viagem.find(params[:id])
     @motorista = Motorista.find(@viagem.motorista_id) if not @viagem.motorista_id.nil?
     @veiculo = Veiculo.find(@viagem.veiculo_id) if not @viagem.veiculo_id.nil?
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @viagem }
-    end
   end
 
-  # GET /viagens/new
-  # GET /viagens/new.xml
   def new
     @viagem = Viagem.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @viagem }
-    end
   end
 
-  # GET /viagens/1/edit
   def edit
     @viagem = Viagem.find(params[:id])
     data_partida = @viagem.data_partida
@@ -69,25 +46,17 @@ class ViagensController < ApplicationController
                       ]
   end
 
-  # POST /viagens
-  # POST /viagens.xml
   def create
     @viagem = Viagem.new(params[:viagem])
 
-    respond_to do |format|
-      if @viagem.save
-        flash[:notice] = 'Viagem was successfully created.'
-        format.html { redirect_to(@viagem) }
-        format.xml  { render :xml => @viagem, :status => :created, :location => @viagem }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @viagem.errors, :status => :unprocessable_entity }
-      end
+    if @viagem.save
+      flash[:notice] = 'Viagem was successfully created.'
+      redirect_to(@viagem)
+    else
+      render :action => "new"
     end
   end
 
-  # PUT /viagens/1
-  # PUT /viagens/1.xml
   def update
     @viagem = Viagem.find(params[:id])
     params[:viagem][:data_partida] = data_str_ou_nil(params[:viagem][:data_partida])
@@ -120,16 +89,10 @@ class ViagensController < ApplicationController
     end
   end
 
-  # DELETE /viagens/1
-  # DELETE /viagens/1.xml
   def destroy
     @viagem = Viagem.find(params[:id])
     @viagem.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(viagens_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(viagens_url)
   end
 
   def cancelar_viagem
