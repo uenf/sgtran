44dd2f8ec0aced7acde4d3208ecd185862c53110
Eduardo Hertz de Almeida Gomes <hertz@AdaLovelace-desktop.(none)>
2010-06-11 09:45:37 -0300

    Configurado o javascript para o campo de busca de viagens (adicionado o campo 'Placa').

--- public/javascripts/busca.js
+++ public/javascripts/busca.js
@@ -33,24 +33,35 @@ function busca_viagem(campo)
         document.getElementById("box_data_de_partida").className = 'visivel'
         document.getElementById("box_data_de_chegada").className = 'invisivel'
         document.getElementById("box_motorista").className = 'invisivel'
+        document.getElementById("box_placa").className = 'invisivel'
     }
     else if (campo.value == "Data de chegada")
     {
         document.getElementById("box_data_de_partida").className = 'invisivel'
         document.getElementById("box_data_de_chegada").className = 'visivel'
         document.getElementById("box_motorista").className = 'invisivel'
+        document.getElementById("box_placa").className = 'invisivel'        
     }
     else if (campo.value == "Motorista")
     {
         document.getElementById("box_data_de_partida").className = 'invisivel'
         document.getElementById("box_data_de_chegada").className = 'invisivel'
         document.getElementById("box_motorista").className = 'visivel'
+        document.getElementById("box_placa").className = 'invisivel'        
+    }
+    else if (campo.value == "Placa")
+    {
+        document.getElementById("box_data_de_partida").className = 'invisivel'
+        document.getElementById("box_data_de_chegada").className = 'invisivel'
+        document.getElementById("box_motorista").className = 'invisivel'
+        document.getElementById("box_placa").className = 'visivel'          
     }
     else
     {
         document.getElementById("box_data_de_partida").className = 'invisivel'
         document.getElementById("box_data_de_chegada").className = 'invisivel'
         document.getElementById("box_motorista").className = 'invisivel'
+        document.getElementById("box_placa").className = 'invisivel'        
     }
 }
 
