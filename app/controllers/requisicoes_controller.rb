37ec388fb3b1bd1c186fcf9afc4d3507b35fbd08
Eduardo Hertz de Almeida Gomes <hertz@AdaLovelace-desktop.(none)>
2010-06-14 15:28:14 -0300

    E-mails descomentados e email_configuration modificado.

--- app/controllers/requisicoes_controller.rb
+++ app/controllers/requisicoes_controller.rb
@@ -60,7 +60,7 @@ class RequisicoesController < ApplicationController
     if @requisicao.length == 1
       if @requisicao[IDA].valid?
         session[:requisicao] = [@requisicao[IDA].id]
-        ##Confirmacao.deliver_email_confirmacao_de_cadastro_de_requisicao(@requisicao)
+        Confirmacao.deliver_email_confirmacao_de_cadastro_de_requisicao(@requisicao)
         redirect_to(confirmar_requisicao_path)
       else
         @requisicao = @requisicao[IDA]
@@ -70,7 +70,7 @@ class RequisicoesController < ApplicationController
       if @requisicao[IDA].valid?
         if @requisicao[VOLTA].valid?
           session[:requisicao] = [@requisicao[IDA].id, @requisicao[VOLTA].id]
-          ##Confirmacao.deliver_email_confirmacao_de_cadastro_de_requisicao(@requisicao)
+          Confirmacao.deliver_email_confirmacao_de_cadastro_de_requisicao(@requisicao)
           redirect_to(confirmar_requisicao_path)
         else
           @requisicao = @requisicao[VOLTA]
@@ -188,7 +188,7 @@ class RequisicoesController < ApplicationController
       if @viagem.valid?
         @viagem.save!
         @requisicao.aceitar @viagem
-        ##Confirmacao.deliver_enviar_email_aceitar(corpo_do_email, destinatarios, @requisicao)
+        Confirmacao.deliver_enviar_email_aceitar(corpo_do_email, destinatarios, @requisicao)
         redirect_to :controller => "viagem", :action => "show", :id => @viagem.id
       else
         session[:viagem] = @viagem
@@ -200,7 +200,7 @@ class RequisicoesController < ApplicationController
         @viagem = @requisicao.aceitar_com_viagem_existente(params[:id_da_viagem])
         if @viagem
           redirect_to :controller => "viagem", :action => "show", :id => @viagem.id
-          ##Confirmacao.deliver_enviar_email_aceitar(corpo_do_email, destinatarios, @requisicao)
+          Confirmacao.deliver_enviar_email_aceitar(corpo_do_email, destinatarios, @requisicao)
         else
           session[:requisicao] = {:id => @requisicao.id, :erro => " não foi selecionada."}
           redirect_to :action => "aceitar"
@@ -224,7 +224,7 @@ class RequisicoesController < ApplicationController
       render :layout => "requisicoes"
       session[:requisicao] = @requisicao
       #incluindo linha para enviar o emailtesteuenf para ailton informando que professor cancelou a requisição
-      ##Confirmacao.deliver_email_cancelamento_professor(@requisicao)
+      Confirmacao.deliver_email_cancelamento_professor(@requisicao)
     else
       render :action => "cancelar_requisicao", :layout => "requisicoes"
     end
@@ -264,7 +264,7 @@ class RequisicoesController < ApplicationController
     corpo_email = params[:corpo_email]
     destinatarios = params[:destinatarios]
     if @requisicao.rejeitar(motivo)
-      ##Confirmacao.deliver_enviar_email(corpo_email, destinatarios, @requisicao)
+      Confirmacao.deliver_enviar_email(corpo_email, destinatarios, @requisicao)
       redirect_to requisicao_path(@requisicao)
     else
       flash[:erro] = "Erro ao rejeitar a requisição. " + @requisicao.errors.full_messages.to_sentence
@@ -288,7 +288,7 @@ class RequisicoesController < ApplicationController
     if @requisicao.esta_aceita?
       retorno = @requisicao.cancelar_requisicao motivo.to_i, corpo_do_email, destinatarios
       if retorno
-        ##Confirmacao.deliver_enviar_email(corpo_do_email, destinatarios, @requisicao) if retorno
+        Confirmacao.deliver_enviar_email(corpo_do_email, destinatarios, @requisicao) if retorno
       end
     else
       flash[:erro] = "A requisição deve estar no estado 'Aceita' para ser cancelada."
