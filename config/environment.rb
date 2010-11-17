0b141499acc37338416c20603c7dfa1c4eceaed5
Eduardo Hertz <eduardohertz@gmail.com>
2010-11-17 01:36:21 -0200

    Bundle installed.

--- config/environment.rb
+++ config/environment.rb
@@ -26,16 +26,6 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem "brazilian-rails", :version => '2.1.13'
-  config.gem "calendar_date_select", :version => '1.16.1'
-  config.gem "authlogic", :version => '2.1.6'
-  config.gem "acl9", :source => "http://gemcutter.org", :lib => "acl9", :version => '0.12.0'
-  config.gem "mysql", :version => '2.8.1'
-  config.gem "factory_girl", :version => '1.3.2'
-  config.gem "will_paginate", :version => '~> 2.3.11', :source => 'http://gemcutter.org'
-  config.gem 'i18n', :version => "0.4.2"
-
-
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,8 +27,3 @@ config.action_mailer.delivery_method = :test
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
 
-
-config.gem "remarkable_rails", :lib => false, :version => ">=3.1.10"
-config.gem "factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
-config.gem 'email_spec', :lib => 'email_spec'
-
