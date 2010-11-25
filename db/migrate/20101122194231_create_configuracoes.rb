0b7a201e921602d661be3e05d5abc47610f06e68
Eduardo Hertz <eduardohertz@gmail.com>
2010-11-25 14:44:03 -0200

    Migrate fixed.

--- db/migrate/20101122194231_create_configuracoes.rb
+++ db/migrate/20101122194231_create_configuracoes.rb
@@ -1,11 +1,12 @@
 class CreateConfiguracoes < ActiveRecord::Migration
   def self.up
     create_table :configuracoes do |t|
-      t.string :orgao_utilizador
+      t.string :orgao_utilizador, :default => "Universidade Estadual do Norte Fluminense Darcy Ribeiro"
       t.boolean :formulario_ativo, :default => true
 
       t.timestamps
     end
+    Configuracao.create!
   end
 
   def self.down
