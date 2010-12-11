3a238c183200a619a99b48af6da67e0e3165d091
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2010-12-11 21:31:10 -0200

    Removidos comentarios

--- app/models/motorista.rb
+++ app/models/motorista.rb
@@ -74,23 +74,6 @@ class Motorista < ActiveRecord::Base
     return motoristas_desocupados
   end
 
-#  def distancia_percorrida_entre inicio, fim
-#    inicio =  inicio.to_date
-#    fim =  fim.to_date
-
-#    bdts = Bdt.find_by_sql(["SELECT * FROM (SELECT * from bdts WHERE \
-#             :inicio <= data_partida AND :fim >= data_recolhimento) as t1 JOIN
-#             (SELECT viagem_id from motoristas_viagens WHERE motorista_id = :id) as t2 \
-#             ON t1.viagem_id = t2.viagem_id",
-#             {:inicio => inicio, :fim => fim, :id => self.id}])
-
-#    soma_km = 0
-#    bdts.each do |bdt|
-#      soma_km += bdt.distancia_percorrida
-#    end
-#    soma_km
-#  end
-
   def distancia_percorrida_entre inicio, fim
     inicio =  inicio.to_date
     fim =  fim.to_date
