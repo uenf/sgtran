3a92bfd9c4aab1e0cbb1846cdde6a9679382cf65
Hugo Maia Vieira <hugomaiavieira@gmail.com>
2010-12-13 01:32:46 -0200

    Refatorado calculo de quilometragem geral do relatorio

--- app/controllers/relatorios_controller.rb
+++ app/controllers/relatorios_controller.rb
@@ -34,35 +34,29 @@ class RelatoriosController < ApplicationController
         # XXX: Tem que corrigir o odf-report para poder funcionar o código abaixo
         # r.add_image 'CABECALHO', "#{RAILS_ROOT}/public/images/cabecalho_relatorio.eps"
 
-        # XXX: Está sendo feito 12x mais acessos a disco do que o necessário.
-        # Isso devido à implementação do odf-report que não tem um método
+        km_geral = [0]
+        for mes in 1..12 do
+          data = "01/#{mes}/#{@ano}".to_date
+          km_geral << milhar(Bdt.distancia_percorrida_entre(data, data.end_of_month))
+        end
+
+        # XXX: Está sendo feito 12x iterações do que o necessário. Isso devido
+        # à implementação do odf-report que não tem um método
         # add_row para casos como esse.
         r.add_table("TABELA_KM_GERAL", (1..13).to_a, :header=>true) do |t|
-          t.add_column('1') { |mes| milhar(Bdt.distancia_percorrida_entre(
-                                    "01/01/#{@ano}", "31/01/#{@ano}"))}
-          t.add_column('2') { |mes| milhar(Bdt.distancia_percorrida_entre(
-                                    "01/02/#{@ano}", "1/02/#{@ano}".to_date.end_of_month))}
-          t.add_column('3') { |mes| milhar(Bdt.distancia_percorrida_entre(
-                                    "01/03/#{@ano}", "31/03/#{@ano}"))}
-          t.add_column('4') { |mes| milhar(Bdt.distancia_percorrida_entre(
-                                    "01/04/#{@ano}", "30/04/#{@ano}"))}
-          t.add_column('5') { |mes| milhar(Bdt.distancia_percorrida_entre(
-                                    "01/05/#{@ano}", "31/05/#{@ano}"))}
-          t.add_column('6') { |mes| milhar(Bdt.distancia_percorrida_entre(
-                                    "01/06/#{@ano}", "30/06/#{@ano}"))}
-          t.add_column('7') { |mes| milhar(Bdt.distancia_percorrida_entre(
-                                    "01/07/#{@ano}", "31/07/#{@ano}"))}
-          t.add_column('8') { |mes| milhar(Bdt.distancia_percorrida_entre(
-                                    "01/08/#{@ano}", "31/08/#{@ano}"))}
-          t.add_column('9') { |mes| milhar(Bdt.distancia_percorrida_entre(
-                                    "01/09/#{@ano}", "30/09/#{@ano}"))}
-          t.add_column('10') { |mes| milhar(Bdt.distancia_percorrida_entre(
-                                    "01/10/#{@ano}", "31/10/#{@ano}"))}
-          t.add_column('11') { |mes| milhar(Bdt.distancia_percorrida_entre(
-                                    "01/11/#{@ano}", "30/11/#{@ano}"))}
-          t.add_column('12') { |mes| milhar(Bdt.distancia_percorrida_entre(
-                                    "01/12/#{@ano}", "31/12/#{@ano}"))}
-          t.add_column('TOTAL') { |mes| milhar(@km_total)}
+          t.add_column('1') { |mes| km_geral[1] }
+          t.add_column('2') { |mes| km_geral[2] }
+          t.add_column('3') { |mes| km_geral[3] }
+          t.add_column('4') { |mes| km_geral[4] }
+          t.add_column('5') { |mes| km_geral[5] }
+          t.add_column('6') { |mes| km_geral[6] }
+          t.add_column('7') { |mes| km_geral[7] }
+          t.add_column('8') { |mes| km_geral[8] }
+          t.add_column('9') { |mes| km_geral[9] }
+          t.add_column('10') { |mes| km_geral[10] }
+          t.add_column('11') { |mes| km_geral[11] }
+          t.add_column('12') { |mes| km_geral[12] }
+          t.add_column('TOTAL') { |mes| milhar(@km_total) }
         end
 
         r.add_table("TABELA_KM_CENTRO", @centros, :header=>true) do |t|
