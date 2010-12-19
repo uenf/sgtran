efcb5669e4f63a2feade4c35265b794db7a8702b
Eduardo Hertz <eduardohertz@gmail.com>
2010-12-19 11:20:10 -0200

    Javascript funcionando no Chrome e no Firefox. Verificar no IE.

--- public/javascripts/new_requisicao.js
+++ public/javascripts/new_requisicao.js
@@ -1,7 +1,11 @@
-function observacoesObrigatorio(cssClass) {
-    document.getElementById("observacao_label").className = cssClass
-}
-function removeObservacoesObrigatorio(cssClass) {
-    document.getElementById("observacao_label").className = cssClass
+function verificarObservacao() {
+    var id = document.getElementById('requisicao_objetivo_de_reserva_id').options[document.getElementById('requisicao_objetivo_de_reserva_id').selectedIndex].id
+    if (id.search(/nao_obrigatorio/)) {
+        document.getElementById('observacao_label').className = 'obrigatorio'
+    }
+    else
+    {
+        document.getElementById('observacao_label').className = ''
+    }
 }
 
